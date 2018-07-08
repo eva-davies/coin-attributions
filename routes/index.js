5eb4d9bcaa5151c0ea55fbee226b6377b54f2237
echipana <echipana@carecloud.com>
2018-07-08 16:55:01 -0400

    cors

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,11 @@
 const healthCheck = require('./health-check');
 
 module.exports = (app) => {
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
   app.use('/health-check', healthCheck);
 };
